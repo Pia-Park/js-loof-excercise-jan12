ed5bd65c05b5375cd6aa56a70adcea19d336a8a2
仁愛 朴 <inae1park@jinai-no-MacBook-Pro.local>
2021-01-12 11:07:37 -0800

    first commit

---
+++ todo.js
@@ -0,0 +1,11 @@
+let input = prompt('What would you like to do?');
+
+//optional to add dummy data inside todos
+const todos = ['Collect Chicken Eggs','Clean Litter Box'];
+
+//start with a while loop that checks for quit input
+while(){
+    //if else conditions
+}
+
+console.log('OK QUIT THE APP');
